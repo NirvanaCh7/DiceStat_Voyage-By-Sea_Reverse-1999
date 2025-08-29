b1225339af1d49344decd92ed53fd507ecc6afc0
NirvanaCh7 <nirvanach7@outlook.com>
2025-08-29 18:33:17 +0800

    Add i18n tailwindcss shadcnvue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">{{ $t('welcome') }}</h1>
+    <Button>111</Button>
   </div>
 </template>
+<script setup>
+const { locales, setLocale} = useI18n()
+setLocale('zh-hans')
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: true, 
-  target: 'static',
-  router: {
-    base: '/DiceStat_Voyage-By-Sea_Reverse-1999/', 
-  }
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  // ssr: true,
+  // target: 'static',
+
+  // router: {
+  //   base: '/DiceStat_Voyage-By-Sea_Reverse-1999/', 
+  // },
+
+  modules: ['nuxt-echarts', '@nuxtjs/i18n','shadcn-nuxt'],
+  shadcn: {
+    
+    prefix: '',
+    
+    componentDir: './app/components/ui'
+  },
+  i18n: {
+    locales: [
+      // { code: 'en', iso: 'en', file: 'en.json', name: 'English' },
+      { code: 'zh-hans', iso: 'zh-hans', file: 'zh-hans.json', name: '简体中文' },
+    ],
+    defaultLocale: 'zh-han',
+    strategy: 'prefix',
+  },
 })
